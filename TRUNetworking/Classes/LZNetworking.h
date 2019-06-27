eaf5d1ef0011b172dbeceb3ed9907af153a52091
chenqiang <chenqiang2@hyc.cn>
2019-06-27 16:46:50 +0800

    init networking project

---
+++ TRUNetworking/Classes/LZNetworking.h
@@ -0,0 +1,16 @@
+//
+//  LZNetworking.h
+//  FBSnapshotTestCase
+//
+//  Created by Chen on 2019/6/27.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LZNetworking : NSObject
+-(void)test;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TRUNetworking/Classes/LZNetworking.m
@@ -0,0 +1,14 @@
+//
+//  LZNetworking.m
+//  FBSnapshotTestCase
+//
+//  Created by Chen on 2019/6/27.
+//
+
+#import "LZNetworking.h"
+
+@implementation LZNetworking
+-(void)test{
+    NSLog(@"test");
+}
+@end
